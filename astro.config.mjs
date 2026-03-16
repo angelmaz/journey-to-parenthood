c1773cbe8d23d571e935caeba9baddd9fef3db75
Angelika Mazurek <angelika.a.mazurek@gmail.com>
2026-03-16 16:36:08 -0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,5 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://journey-to-parenthood.com',
-
-  integrations: [
-    sitemap()
-  ]
+  integrations: [sitemap()]
 });
